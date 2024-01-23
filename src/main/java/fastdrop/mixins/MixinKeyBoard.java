5c255bc4a414a4302c02c06bb7e0f3d0163e0837
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-01-24 01:07:12 +0300

    Update MixinKeyBoard.java

--- src/main/java/fastdrop/mixins/MixinKeyBoard.java
+++ src/main/java/fastdrop/mixins/MixinKeyBoard.java
@@ -13,10 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinKeyBoard {
     @Inject(method = "onKey", at = @At("HEAD"))
     private void onKey(long windowPointer, int key, int scanCode, int action, int modifiers, CallbackInfo ci) {
-        if (action == 0 && key == 320)
+        if (action == 0 && key == 320 && MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().player.networkHandler != null && MinecraftClient.getInstance().world != null) {
             for (int i = 5; i <= 45; i++) {
                 MinecraftClient.getInstance().interactionManager.clickSlot(MinecraftClient.getInstance().player.currentScreenHandler.syncId, i, 1, SlotActionType.THROW, MinecraftClient.getInstance().player);
             }
-        MinecraftClient.getInstance().player.networkHandler.sendPacket(new CloseHandledScreenC2SPacket(MinecraftClient.getInstance().player.currentScreenHandler.syncId));
+            MinecraftClient.getInstance().player.networkHandler.sendPacket(new CloseHandledScreenC2SPacket(MinecraftClient.getInstance().player.currentScreenHandler.syncId));
+        }
     }
 }
